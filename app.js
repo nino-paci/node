1130edcd130078a7ac4bb419010b8aed59162dec
nino-paci <64863384+nino-paci@users.noreply.github.com>
2020-05-06 11:03:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const app = express()
 
 app.get('/',(req,res) => {
-		res.send('Welcole to the homepage Baby')
+		res.send('Welcome to the homepage Baby')
 })
 app.listen(8000, () =>{
 	console.log('Wazzzappppppppp')
